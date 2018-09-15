67e04882a2aab26b995ebb265a20c50771e4f073
Julian Andres Maldonado Aponte <Jmaldonadoa@intergrupo.com>
2018-09-15 16:00:33 -0500

    Se agrega proxy

--- Hackathon.App/Activities/LoginActivity.cs
+++ Hackathon.App/Activities/LoginActivity.cs
@@ -9,17 +9,31 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Proxy.App;
 
 namespace Hackathon.App.Activities
 {
     [Activity(Label = "LoginActivity")]
     public class LoginActivity : Activity
     {
+        private ServiceClient serviceClient;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             SetContentView(Resource.Layout.login_layout);
+
+            serviceClient = new ServiceClient();
+
+            var btLogin = FindViewById<Button>(Resource.Id.BtLogin);
+            btLogin.Click += BtLogin_Click;
+        }
+
+        private void BtLogin_Click(object sender, EventArgs e)
+        {
+            //TODO Login 
+            serviceClient.Login("user1","12345");
         }
     }
 }
---
+++ Proxy.App/ServiceClient.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy.App
+{
+    public class ServiceClient
+    {
+        protected Uri ServiceUrl { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserServiceClient"/> class.
+        /// </summary>
+        public ServiceClient()
+        {
+            ServiceUrl = new Uri("");
+        }
+
+        /// <summary>
+        /// Authenticate User.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<bool> Login(string user, string password)
+        {
+            try
+            {
+                var urlService = new Uri($"{ServiceUrl}/AuthenticateUser");
+                var client = new HttpClient();
+                Dictionary<string, string> data = new Dictionary<string, string>();
+                data.Add("user", user);
+                data.Add("password", password);
+
+
+                var json = JsonConvert.SerializeObject(data);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = null;
+                response = await client.PostAsync(urlService, content);
+
+                var responseString = string.Empty;
+                if (response.IsSuccessStatusCode)
+                {
+                    responseString = await response.Content.ReadAsStringAsync();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
